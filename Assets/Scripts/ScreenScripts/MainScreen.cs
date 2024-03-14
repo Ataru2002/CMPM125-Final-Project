2b4608d625782609e1f8ff0b69fc7243ccd26006
wichapas <wpichetp@ucsc.edu>
2024-03-13 18:02:38 -0700

    UI stuff + finishing touches

--- Assets/Scripts/ScreenScripts/MainScreen.cs
+++ Assets/Scripts/ScreenScripts/MainScreen.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class mainScreen : MonoBehaviour
 {
-    public void gameplay()
+    public GameObject panel;
+
+    void Start(){
+        
+        if(panel != null){
+            panel.SetActive(false);
+        }
+    }
+    public void levelOne(){
+        SceneManager.LoadScene("level1");
+    }
+    public void levelTwo()
     {
         SceneManager.LoadScene("level2");
     }
@@ -16,5 +28,12 @@ public class mainScreen : MonoBehaviour
     public void instructions()
     {
         SceneManager.LoadScene("instructionScreen");
+    }
+    public void showPanel(){
+        panel.SetActive(true);
+    }
+
+    public void hidePanel(){
+        panel.SetActive(false);
     }
 }
